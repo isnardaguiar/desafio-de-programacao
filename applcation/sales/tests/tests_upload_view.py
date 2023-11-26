2c15db68c2075ea609b6080f9c47d26b4a046f30
Felipe Villela <felipeavillela@gmail.com>
2023-11-26 20:51:45 -0300

    Adicionado testes para view upload

---
+++ applcation/sales/tests/tests_upload_view.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+class UploadFileViewTest(TestCase):
+    def test_upload_file_view_get(self):
+        url = reverse('sales:upload')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'upload.html')
+
+    def test_upload_file_view_post(self):
+        url = reverse('sales:upload')
+        file_content = "purchaser name\titem description\titem price\tpurchase count\tmerchant address\tmerchant name\nJohn\tItem ABC\t10.0\t2\tAddress XYZ\tMerchant XYZ"
+        file = SimpleUploadedFile("file.tab", file_content.encode('utf-8'))
+
+        response = self.client.post(url, {'file': file})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'result.html')
+

04e93aa29517c07a5912377cdd7eecd26a2929d5
Felipe Villela <felipeavillela@gmail.com>
2023-11-26 18:28:17 -0300

    Funções para processar o arquivo recebido e salvar no banco de dados

--- applcation/sales/urls.py
+++ applcation/sales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, upload_file
 
 app_name = 'sales'
 
 urlpatterns = [
-    path('hello/', hello_world, name='upload'),
+    path('hello/', hello_world, name='hello'),
+    path('upload/', upload_file, name='upload'),
 ]

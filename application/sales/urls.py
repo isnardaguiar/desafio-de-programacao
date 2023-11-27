8894eb18154d5dd69b26833abd03eafbdc0b9c85
Seu Nome <seu@email.com>
2023-11-27 18:45:07 -0300

    Removidas funções que não são usadas

--- application/sales/urls.py
+++ application/sales/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import hello_world, upload_file
+from .views import upload_file
 
 app_name = 'sales'
 
 urlpatterns = [
-    path('hello/', hello_world, name='hello'),
     path('upload/', upload_file, name='upload'),
 ]
